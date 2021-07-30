d2f0a7da70397acbff095fdcd44dc2bcdbe8be91
mstaniszewski <m.staniszewski@sente.pl>
2021-07-30 12:01:59 +0200

    1_4 Kolejkakolowa

--- CsharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
@@ -10,6 +10,26 @@ namespace _1_TypyGeneryczne
 	{
 		static void Main(string[] args)
 		{
+			var kolejka = new KolejkaKolowa(pojemnosc: 3);
+
+			while (true)
+			{
+				var wartosc = 0.0;
+				var wartoscWejsciowa = Console.ReadLine();
+
+				if (double.TryParse(wartoscWejsciowa, out wartosc))
+				{
+					kolejka.Zapisz(wartosc);
+					continue;
+				}
+				break;
+			}
+
+			Console.WriteLine("w naszej kolejce jest: ");
+			while (!kolejka.JestPusty)
+			{
+				Console.WriteLine("\t\t" + kolejka.Czytaj());
+			}
 		}
 	}
 }
