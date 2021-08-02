9594916e8d8bcac714fbb38082a7f533b15e7696
mstaniszewski <m.staniszewski@sente.pl>
2021-08-02 15:52:16 +0200

    LinkedList testy

---
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczneTests/LinkedListTest.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2_KolekcjeGeneryczneTests
+{
+    [TestClass]
+    public class LinkedlistTest
+    {
+        [TestMethod]
+        public void DodawaniePoElemencie()
+        {
+            var lista = new LinkedList<string>();
+
+            lista.AddFirst("Marcin");
+            lista.AddLast("Tomek");
+            lista.AddAfter(lista.First, "Ola");
+
+            Assert.AreEqual("Ola", lista.First.Next.Value);
+        }
+
+        [TestMethod]
+        public void DodawaniePrzedElementem()
+        {
+            var lista = new LinkedList<string>();
+
+            lista.AddFirst("Marcin");
+            lista.AddLast("Tomek");
+            lista.AddBefore(lista.First, "Ola");
+
+            Assert.AreEqual("Ola", lista.First.Value);
+        }
+
+        [TestMethod]
+        public void UsuwanieElementuOstatniego()
+        {
+            var lista = new LinkedList<string>();
+
+            lista.AddFirst("Marcin");
+            lista.AddLast("Tomek");
+            lista.RemoveLast();
+
+            Assert.AreEqual(lista.First, lista.Last);
+        }
+
+        [TestMethod]
+        public void UsuwanieElementu()
+        {
+            var lista = new LinkedList<string>();
+
+            lista.AddFirst("Marcin");
+            lista.AddLast("Tomek");
+            lista.Remove("Marcin");
+
+            Assert.AreEqual("Tomek", lista.Last.Value);
+        }
+
+        [TestMethod]
+        public void CzyListaZawieraElement()
+        {
+            var lista = new LinkedList<string>();
+
+            lista.AddFirst("Marcin");
+            lista.AddLast("Tomek");
+
+            Assert.IsTrue(lista.Contains("Tomek"));
+        }
+
+        [TestMethod]
+        public void Czyszczenielisty()
+        {
+            var lista = new LinkedList<string>();
+
+            lista.AddFirst("Marcin");
+            lista.AddLast("Tomek");
+            lista.Clear();
+
+            Assert.AreEqual(0, lista.Count);
+        }
+
+    }
+}
