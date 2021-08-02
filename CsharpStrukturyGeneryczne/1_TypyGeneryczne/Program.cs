755b775eb49bd1dc8e92e16bd7a0d4540595f824
mstaniszewski <m.staniszewski@sente.pl>
2021-08-02 11:19:43 +0200

    Terminologia

--- CsharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
@@ -9,50 +9,61 @@ namespace _1_TypyGeneryczne
 	class Program
 	{
 		static void Main(string[] args)
-		{
-			var kolejka = new KolejkaKolowa<double>(pojemnosc: 3);
-			var kolejkaInt = new KolejkaKolowa<int>();
-			var kolejkaString = new KolejkaKolowa<string>(1000);
+        {
+            var kolejka = new KolejkaKolowa<double>(pojemnosc: 3);
+            WprowadzanieDanych(kolejka);
+            PrzetwarzanieDanych(kolejka);
 
-			var kolejkaOsob = new KolejkaKolowa<Osoba>();
-			kolejkaOsob.Zapisz(new Osoba { Imie = "Marcin", Nazwisko = "Nowak" });
-			kolejkaOsob.Zapisz(new Osoba { Imie = "Tomek", Nazwisko = "Nowak" });
-			kolejkaOsob.Zapisz(new Osoba { Imie = "Jacek", Nazwisko = "Nowak" });
-			kolejkaOsob.Zapisz(new Osoba { Imie = "Marek", Nazwisko = "Inny" });
+            //var kolejkaInt = new KolejkaKolowa<int>();
+            //var kolejkaString = new KolejkaKolowa<string>(1000);
 
-			while (!kolejkaOsob.JestPusty)
-			{
-				var wynik = kolejkaOsob.Czytaj().Imie;
-				Console.WriteLine(wynik);
-			}
+            //var kolejkaOsob = new KolejkaKolowa<Osoba>();
+            //kolejkaOsob.Zapisz(new Osoba { Imie = "Marcin", Nazwisko = "Nowak" });
+            //kolejkaOsob.Zapisz(new Osoba { Imie = "Tomek", Nazwisko = "Nowak" });
+            //kolejkaOsob.Zapisz(new Osoba { Imie = "Jacek", Nazwisko = "Nowak" });
+            //kolejkaOsob.Zapisz(new Osoba { Imie = "Marek", Nazwisko = "Inny" });
 
-			//while (true)
-			//{
-			//	var wartosc = 0.0;
-			//	var wartoscWejsciowa = Console.ReadLine();
-
-			//	if (double.TryParse(wartoscWejsciowa, out wartosc))
-			//	{
-			//		kolejka.Zapisz(wartosc);
-			//		continue;
-			//	}
-			//	break;
-			//}
+            //while (!kolejkaOsob.JestPusty)
+            //{
+            //	var wynik = kolejkaOsob.Czytaj().Imie;
+            //	Console.WriteLine(wynik);
+            //}
 
 
-			//var suma = 0.0;
-			//Console.WriteLine("w naszej kolejce jest: ");
-			//while (!kolejka.JestPusty)
-   //         {
-			//	suma += kolejka.Czytaj();
-   //         }
         }
-	}
 
-	public class Osoba
-    {
-		public string Imie { get; set; }
+        private static void PrzetwarzanieDanych(KolejkaKolowa<double> kolejka)
+        {
+            var suma = 0.0;
+            Console.WriteLine("w naszej kolejce jest: ");
+            while (!kolejka.JestPusty)
+            {
+                suma += kolejka.Czytaj();
+            }
+            Console.WriteLine(suma);
+        }
 
-		public string Nazwisko { get; set; }
+        private static void WprowadzanieDanych(KolejkaKolowa<double> kolejka)
+        {
+            while (true)
+            {
+                var wartosc = 0.0;
+                var wartoscWejsciowa = Console.ReadLine();
+
+                if (double.TryParse(wartoscWejsciowa, out wartosc))
+                {
+                    kolejka.Zapisz(wartosc);
+                    continue;
+                }
+                break;
+            }
+        }
     }
+
+	//public class Osoba
+ //   {
+	//	public string Imie { get; set; }
+
+	//	public string Nazwisko { get; set; }
+ //   }
 }
