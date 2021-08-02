430c631c5fd82431757b460aee691c8b52467889
mstaniszewski <m.staniszewski@sente.pl>
2021-08-02 14:48:10 +0200

    HashSet testy

---
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczneTests/HashSetTest.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2_KolekcjeGeneryczneTests
+{
+    [TestClass]
+    public class HashSetTest
+    {
+        [TestMethod]
+        public void IntersectSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            set1.IntersectWith(set2);
+
+            Assert.IsTrue(set1.SetEquals(new[] { 2, 3 }));
+        }
+
+        [TestMethod]
+        public void UnionSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            set1.UnionWith(set2);
+
+            Assert.IsTrue(set1.SetEquals(new[] { 1, 2, 3, 4 }));
+        }
+
+        [TestMethod]
+        public void SymmetricExceptSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            set1.SymmetricExceptWith(set2);
+
+            Assert.IsTrue(set1.SetEquals(new[] { 1, 4 }));
+        }
+
+        [TestMethod]
+        public void RemoveFromSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+
+            set1.Remove(1);
+
+            Assert.AreEqual(2, set1.Count);
+        }
+
+        [TestMethod]
+        public void RemoveWhereSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+
+            set1.RemoveWhere(x => x > 1);
+
+            Assert.AreEqual(1, set1.Count);
+        }
+
+        [TestMethod]
+        public void ContainsSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+
+            Assert.IsTrue(set1.Contains(3));
+        }
+
+
+    }
+}
