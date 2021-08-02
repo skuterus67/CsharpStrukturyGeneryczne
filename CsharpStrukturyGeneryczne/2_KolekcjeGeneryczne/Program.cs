d98c14f199f7068b006557049757ef81b626080f
mstaniszewski <m.staniszewski@sente.pl>
2021-08-02 11:56:19 +0200

    Tworzenie Kolekcji generycznych - tablica

--- CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
@@ -10,6 +10,26 @@ namespace _2_KolekcjeGeneryczne
 	{
 		static void Main(string[] args)
 		{
+			Pracownik[] pracownicy = new Pracownik[]
+			{
+				new Pracownik { Imie = "Marcin", Nazwisko = "Nowak"},
+				new Pracownik { Imie = "Jan", Nazwisko = "Nowak" },
+				new Pracownik { Imie = "Tomek", Nazwisko = "Nowak" },
+			};
+
+			foreach(var pracownik in pracownicy)
+            {
+				Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
+            }
+
+			for(int i = 0; i < pracownicy.Length; i++)
+            {
+				Console.WriteLine(pracownicy[i].Imie + " " + pracownicy[i].Nazwisko);
+			}
+
+			Array.Resize(ref pracownicy, 10);
+			pracownicy[9] = new Pracownik { Imie = "Anatol", Nazwisko = "Koszyk" };
+
 		}
 	}
 }
